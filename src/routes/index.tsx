afcc282ea4680ea352403a3ef299047dc91e2c75
MishuBufnita <39066417+MishuBufnita@users.noreply.github.com>
2023-09-28 23:06:20 +0300

    Update index.tsx
    hello world to owlprototyping

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,8 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>OwlPrototyping</Title>
+      <h1>Welcome to OwlPrototyping</h1>
       <Counter />
       <p>
         Visit{" "}
