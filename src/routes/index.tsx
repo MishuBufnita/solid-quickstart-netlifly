7dbeb81993b8b841497a215d6893639baeb6ec2e
MishuBufnita <39066417+MishuBufnita@users.noreply.github.com>
2023-09-29 20:59:30 +0300

    logo si css

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,15 @@
 import { Title } from "solid-start";
 import Counter from "~/components/Counter";
 import Logo from "../../public/OwlPrototypingFinalLogo_horizontal_white.png";
+import "./index.css";
 
 export default function Home() {
   return (
     <main>
-      <img src={Logo} />;
+      <img src={Logo} class="Logo" />;
       <Title>Owl Prototyping</Title>
       <h1>Welcome to Owl Prototyping</h1>
       <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
     </main>
   );
 }
