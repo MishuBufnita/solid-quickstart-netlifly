da7b3816ef0e06cb04c84a63335891ec06d24318
MishuBufnita <39066417+MishuBufnita@users.noreply.github.com>
2023-09-29 20:39:30 +0300

    Update index.tsx
    ./public/Owl...

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 import { Title } from "solid-start";
 import Counter from "~/components/Counter";
-import Logo from "../public/OwlPrototypingFinalLogo_horizontal_white.png";
+import Logo from "./public/OwlPrototypingFinalLogo_horizontal_white.png";
 
 export default function Home() {
   return (
     <main>
-      <img src={pdt} alt="Logo" />;
+      <img src={Logo} />;
       <Title>Owl Prototyping</Title>
       <h1>Welcome to Owl Prototyping</h1>
       <Counter />
